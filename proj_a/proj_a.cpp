36ebf3f9285cf96b38338ffb011577889758b8a9
German Korovkin <german.korovkin@iva-tech.ru>
2024-03-27 23:06:42 +0300

    initial

---
+++ proj_a/proj_a.cpp
@@ -0,0 +1,8 @@
+#include "proj_a.hpp"
+
+namespace proj_a
+{
+void do_something()
+{
+}
+}
---
+++ proj_a/proj_a.hpp
@@ -0,0 +1,9 @@
+#ifndef PROJ_A
+#define PROJ_A
+
+namespace proj_a
+{
+void do_something();
+}
+
+#endif //PROJ_A
